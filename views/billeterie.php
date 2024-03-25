a60a0ac9eab48c91efd7922c1e6ab8dfb49a7aa3
Anchana <fatimarajananchana@gmail.com>
2024-03-25 10:58:15 +0100

    j'ai rajouté des horaires vous inquiétez pas

--- views/billeterie.php
+++ views/billeterie.php
@@ -26,7 +26,7 @@
 
     <div class="container">
 
-        <form id="reservation-form">
+        <form id="reservation-form" method="POST">
 
             <div aria-label="breadcrumb" class="breadcrumb">
                 <span id="breadcrumbStep1" class="active">1 - Date et horaire</span>
@@ -48,6 +48,13 @@
                         <option value="09:00">09:00</option>
                         <option value="10:00">10:00</option>
                         <option value="11:00">11:00</option>
+                        <option value="12:00">12:00</option>
+                        <option value="13:00">13:00</option>
+                        <option value="14:00">14:00</option>
+                        <option value="15:00">15:00</option>
+                        <option value="16:00">16:00</option>
+                        <option value="17:00">17:00</option>
+                        <option value="18:00">18:00</option>
                         <!-- Ajoutez d'autres options  -->
                     </select><br><br>
                     <div class="error-message" style="display: none;">Veuillez remplir tous les champs.</div>
