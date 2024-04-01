fd617906a86dd0295f0d62d556edac6b9be2a3fc
Anchana <fatimarajananchana@gmail.com>
2024-04-01 23:11:29 +0200

    bref

--- views/mes-donnees.php
+++ views/mes-donnees.php
@@ -24,9 +24,9 @@
     require '../views/header-bis.php'; ?>
 
 
-    <section class="informations-personnelles">
-    <h1>Informations Personnelles</h1>
-    <form action="/modifier-informations" method="POST">
+    <!-- <section class="informations-personnelles">
+        <form action="/modifier-informations" method="POST">
+        <h1>Informations Personnelles</h1>
         <div class="form-group">
             <label for="nom">Nom:</label>
             <input type="text" id="nom" name="nom" value="<?php echo $_SESSION['nom']; ?>" required>
@@ -50,7 +50,45 @@
             <button type="submit" class="submit-btn">Confirmer les changements</button>
         </div>
     </form>
-</section>
+</section> -->
+
+
+    <section class="infos">
+        <h1>Informations Personnelles</h1>
+        <form action="/modifier-informations" method="POST">
+
+        <div class="noms">
+
+        <div class="nom">
+                <label for="nom">Nom:</label>
+                <input type="text" id="nom" name="nom" value="<?php echo $_SESSION['prenom']; ?>" required>
+        </div>
+
+        <div class="prenom">
+        <label for="prenom">Prénom:</label>
+                <input type="text" id="prenom" name="prenom" value="<?php echo $_SESSION['nom']; ?>" required>
+        </div>
+        </div>
+
+        <div class="mdp">
+            <a href="/changer-mot-de-passe" class="change-password-link">Changer de mot de passe</a>
+        </div>
+        <div class="email">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo $_SESSION['email']; ?>" readonly="readonly">
+        </div>
+        
+        <div class="numero">
+            <label for="numero">Numéro de téléphone</label>
+            <input type="tel" id="numero" name="numero" value="<?php echo $_SESSION['numero']; ?>" required>
+        </div>
+
+
+            <input type="submit" value="Confirmer les changements" class="submit-btn">
+
+        </form>
+
+    </section>
 
 
     <?php require '../views/footer.php'; ?>
