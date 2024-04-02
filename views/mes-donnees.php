12638f619496dd9c39aed842920e924b3d988787
Anchana <fatimarajananchana@gmail.com>
2024-04-02 11:19:53 +0200

    aaah

--- views/mes-donnees.php
+++ views/mes-donnees.php
@@ -24,36 +24,9 @@
     require '../views/header-bis.php'; ?>
 
 
-    <!-- <section class="informations-personnelles">
-        <form action="/modifier-informations" method="POST">
-        <h1>Informations Personnelles</h1>
-        <div class="form-group">
-            <label for="nom">Nom:</label>
-            <input type="text" id="nom" name="nom" value="<?php echo $_SESSION['nom']; ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prénom:</label>
-            <input type="text" id="prenom" name="prenom" value="<?php echo $_SESSION['prenom']; ?>" required>
-        </div>
-        <div class="form-group">
-            <a href="/changer-mot-de-passe" class="change-password-link">Changer de mot de passe</a>
-        </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="<?php echo $_SESSION['email']; ?>" readonly>
-        </div>
-        <div class="form-group">
-            <label for="numero">Numéro de téléphone (+33):</label>
-            <input type="tel" id="numero" name="numero" value="<?php echo $_SESSION['numero']; ?>" required>
-        </div>
-        <div class="form-group">
-            <button type="submit" class="submit-btn">Confirmer les changements</button>
-        </div>
-    </form>
-</section> -->
-
-
     <section class="infos">
+    <div class="border" >
+
         <h1>Informations Personnelles</h1>
         <form action="/modifier-informations" method="POST">
 
@@ -87,6 +60,7 @@
             <input type="submit" value="Confirmer les changements" class="submit-btn">
 
         </form>
+        </div>
 
     </section>
 
